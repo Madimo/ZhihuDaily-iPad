97238b02baa313b3bcecdf9ed79e69610818a3c1
Madimo <MadimoZhang@gmail.com>
2014-08-06 01:09:22 +0800

    Now user can clear cache

--- ZhihuDaily/ZHSettingsViewController.m
+++ ZhihuDaily/ZHSettingsViewController.m
@@ -8,8 +8,11 @@
 
 #import "ZHSettingsViewController.h"
 
-@interface ZHSettingsViewController ()
+#define kActionSheetTagClearCache 1
+
+@interface ZHSettingsViewController () <UIActionSheetDelegate>
 @property (weak, nonatomic) IBOutlet UISwitch *nightModeSwitch;
+@property (weak, nonatomic) IBOutlet UITableViewCell *cacheCell;
 @property (strong, nonatomic) UITapGestureRecognizer *gestureRecognizer;
 @property (strong, nonatomic) NSUserDefaults *defaults;
 @end
@@ -20,6 +23,8 @@
 {
     self.defaults = [NSUserDefaults standardUserDefaults];
     self.nightModeSwitch.on = [self.defaults boolForKey:kUDNightModeKey];
+    
+    [self reloadCacheUsage];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -42,6 +47,62 @@
     [[NSNotificationCenter defaultCenter] postNotificationName:kThemeChangedNotification
                                                         object:@(nightMode)
                                                       userInfo:nil];
+}
+
+- (void)reloadCacheUsage
+{
+    NSInteger currentDiskUsage = [NSURLCache sharedURLCache].currentDiskUsage;
+    self.cacheCell.detailTextLabel.text = [NSString stringWithFormat:@"%.2f MB", currentDiskUsage / 1024.0 / 1024.0];
+}
+
+- (void)clearCache
+{
+    [[NSURLCache sharedURLCache] removeAllCachedResponses];
+    [self reloadCacheUsage];
+}
+
+#pragma mark - UITableViewDelegate
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        return nil;
+    }
+    
+    return indexPath;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    switch (indexPath.row) {
+        case 1: {
+            UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:@"清除缓存"
+                                                               delegate:self
+                                                      cancelButtonTitle:@"取消"
+                                                 destructiveButtonTitle:@"清除缓存"
+                                                      otherButtonTitles:nil];
+            sheet.tag = kActionSheetTagClearCache;
+            [sheet showInView:self.cacheCell];
+            break;
+        }
+        default:
+            break;
+    }
+    
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - UIActionSheetDelegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == actionSheet.cancelButtonIndex) {
+        return;
+    }
+    
+    if (actionSheet.tag == kActionSheetTagClearCache) {
+        [self clearCache];
+    }
 }
 
 - (IBAction)onDoneButtonTapped:(id)sender
